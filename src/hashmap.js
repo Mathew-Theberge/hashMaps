6e1cfb2935b0ba8388f121d82dd7642ed42be5fb
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2025-02-26 21:32:05 -0500

    Refactor code to no longer have non linkedlist nodes containing data

--- src/hashmap.js
+++ src/hashmap.js
@@ -17,17 +17,16 @@ class HashMap {
     return hashCode;
   }
 
-  matchKeys(key) {
-    let i = 0;
+  isKeyEqual(key) {
     const index = this.hash(key);
-    let tempNode = this.buckets[index].head;
-    while (this.buckets[index].listSize > i) {
-      if (tempNode.value[0] === key) {
-        return tempNode;
+    let node = this.buckets[index].head;
+    for (let i = 0; i < this.buckets[index].listSize; i++) {
+      if (node.value[0] === key) {
+        return node;
       }
-      tempNode = tempNode.nextNode;
-      i++;
+      node = node.nextNode;
     }
+    return null;
   }
 
   set(key, value) {
@@ -37,49 +36,26 @@ class HashMap {
       throw new Error("Trying to access index out of bounds");
     }
 
-    if (this.buckets[index] !== undefined) {
-      // we check if the index is a linkedlist because our hashmap
-      //   allows for single peices of data inside a bucket to not
-      // require a linked list
-
-      if (this.buckets[index] instanceof LinkedList) {
-        const matchedKey = this.matchKeys(key);
-        if (matchedKey !== undefined) {
-          console.log(`updating key: ${key} to value: ${value}`);
-          matchedKey.value = [key, value];
-        } else {
-          this.buckets[index].append([key, value]);
-        }
-      }
-      // this else clause is here to let buckets with 1 peice of data
-      // to not need linkedlist objects to store the single element
-      // of data but if a collision does occur a linkedlist obj is created
-      // and the old and incoming data is put into the linkedlist
-      else {
-        if (this.buckets[index][0] === key) {
-          console.log(`updating key: ${key} to value: ${value}`);
-          this.buckets[index] = [key, value];
-        } else {
-          const oldValue = this.buckets[index];
-          this.buckets[index] = new LinkedList();
-          this.buckets[index].append(oldValue);
-          this.buckets[index].append([key, value]);
-        }
+    if (this.buckets[index] instanceof LinkedList) {
+      const node = this.isKeyEqual(key);
+      if (node !== null) {
+        console.log(`updating key: ${key} to value: ${value}`);
+        node.value = [key, value];
+      } else {
+        this.buckets[index].append([key, value]);
       }
     } else {
-      this.buckets[index] = [key, value];
+      this.buckets[index] = new LinkedList();
+      this.buckets[index].append([key, value]);
     }
   }
 
   get(key) {
     const index = this.hash(key);
-    if (this.buckets[index] === undefined) return null;
 
     if (this.buckets[index] instanceof LinkedList) {
-      let matchedKey = this.matchKeys(key);
-      if (matchedKey !== undefined) return matchedKey.value[1];
-    } else if (this.buckets[index][0] === key) {
-      return this.buckets[index][1];
+      let node = this.isKeyEqual(key);
+      if (node !== null) return node.value[1];
     } else return null;
   }
 
@@ -98,15 +74,25 @@ test.set("dog", "brown");
 test.set("elephant", "gray");
 test.set("frog", "green");
 test.set("grape", "purple");
+test.set("grape", "purpl");
 test.set("hat", "black");
+test.set("hat", "whie");
 test.set("ice cream", "white");
 test.set("jacket", "blue");
 test.set("kite", "pink");
 test.set("lion", "golden");
+
 console.log(test.buckets);
 
 console.log(test.buckets[6]);
 console.log(test.has("grape"));
 console.log(test.has("pink"));
+console.log(test.get("frog"));
 
 console.log(test.buckets[11].toString());
+
+test.buckets.forEach((bucket) => {
+  if (bucket instanceof LinkedList) {
+    console.log(bucket.toString());
+  }
+});
